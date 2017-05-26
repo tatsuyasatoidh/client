44bb3d7735fbb9863cad07bca77e30d66c5f4d33
satou <satou.tatsuya@idh-net.co.jp>
2017-05-27 01:41:29 +0900

    FCP アップロード機能の実装

---
+++ PcLogTool/Form2.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class User_Register_Form
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.register_submit_buttom = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label3.Location = New System.Drawing.Point(84, 32)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label4.Location = New System.Drawing.Point(84, 107)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'register_submit_buttom
+        '
+        Me.register_submit_buttom.Location = New System.Drawing.Point(65, 174)
+        Me.register_submit_buttom.Name = "register_submit_buttom"
+        Me.register_submit_buttom.Size = New System.Drawing.Size(75, 23)
+        Me.register_submit_buttom.TabIndex = 4
+        Me.register_submit_buttom.Text = "Button1"
+        Me.register_submit_buttom.UseVisualStyleBackColor = True
+        '
+        'User_Register_Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.register_submit_buttom)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Name = "User_Register_Form"
+        Me.Text = "Form2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents register_submit_buttom As Button
+End Class
---
+++ PcLogTool/Form2.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Runtime.InteropServices
+'mysqlを使う
+Imports MySql.Data.MySqlClient
+
+
+Public Class User_Register_Form
+    'ログインユーザ名取得
+    Dim username As String = System.Environment.UserName
+    'マシン名取得
+    Dim userspc As String = System.Environment.MachineName
+    '現在の日付を取得する
+    Dim dtToday As DateTime = DateTime.Today
+
+    Private Sub User_Register_Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label3.Text = username
+        Label4.Text = userspc
+    End Sub
+
+    Private Sub register_submit_buttom_Click(sender As Object, e As EventArgs) Handles register_submit_buttom.Click
+
+        ' 1.接続文字列を作成する
+        Dim Builder = New MySqlConnectionStringBuilder()
+        ' データベースに接続するために必要な情報をBuilderに与える
+        Builder.Server = "localhost"
+        Builder.Port = 3306
+        Builder.UserID = "root"
+        Builder.Database = "pclog"
+        Dim ConStr = Builder.ToString()
+
+        ' 2　　.データベースに接続するためのコネクションを準備して、実際につなぐ
+        Dim Con As New MySqlConnection
+        Con.ConnectionString = ConStr
+        Con.Open()
+
+
+        'pclog_idを使ってファイルを格納
+        Dim myInsertQuery As String = "INSERT INTO `user`(`user_name`, `machine_name`) VALUES(?val5,?val6)"
+        Dim myCommand As New MySqlCommand(myInsertQuery, Con)
+
+        'pclog_id
+        myCommand.Parameters.AddWithValue("?val5", Label3.Text)
+        'logファイル
+        myCommand.Parameters.AddWithValue("?val6", Label4.Text)
+
+        'SQLを実行します
+        myCommand.ExecuteNonQuery()
+
+        ' 6.データベースの切断
+        Con.Close()
+
+        Dim form As New Form1()
+        form.ShowDialog(Me)
+
+    End Sub
+
+End Class
